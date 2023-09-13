e5fc3f65365ba240846913a24e01ec6fb8ac83de
iansexton <mydesignguy@gmail.com>
2023-09-12 21:11:45 -0400

    ga-BabyBumper direction of baby is working

--- script.js
+++ script.js
@@ -8,23 +8,81 @@
  *
  *
  */
-const items = document.querySelectorAll('.item');
-console.log('items', items);
-items.forEach((item) => {
-  console.log(item);
+const items = document.querySelectorAll( '.item' );
+const babyIcon = document.getElementById( 'baby' );
+const objLocationMap = {};
+console.log( 'items', items );
+items.forEach( ( item, i ) => {
+    console.log( 'item', i, item.dataset.name );
 
-  item.addEventListener('click', function () {
-    this.classList.toggle('tip-left');
-  });
-});
+    item.addEventListener( 'click', function () {
+        this.classList.toggle( 'tip-left' );
+    } );
+
+    objLocationMap[item.dataset.name] = {top: item.getBoundingClientRect().top, bottom: item.getBoundingClientRect().bottom, left: item.getBoundingClientRect().left, right: item.getBoundingClientRect().right};
+} );
+console.log( 'locations', objLocationMap );
+
 class baby {
-  constructor(name) {
-    this.name = name;
-  }
+    constructor( name ) {
+        this.name = name;
+        this.babyPosition = {top: babyIcon.offsetTop, left: babyIcon.offsetLeft}
+    }
 
-  move() {}
+    move( e ) {
+        e = e || window.Event;
 
-  addToScore() {}
 
-  isTouching() {}
+        console.log( 'move', e.key );
+        switch ( e.key ) {
+            // up arrow
+            case 'ArrowUp':
+                this.babyPosition.top = Math.ceil( this.babyPosition.top ) - 10;
+                babyIcon.className = '';
+                babyIcon.classList.add( 'going-up' );
+                babyIcon.style.top = this.babyPosition.top.toString() + 'px';
+                break;
+            // down arrow
+            case 'ArrowDown':
+                this.babyPosition.top = Math.floor( this.babyPosition.top ) + 10;
+                babyIcon.className = '';
+                babyIcon.classList.add( 'going-down' );
+                babyIcon.style.top = this.babyPosition.top.toString() + 'px';
+                break;
+            // left arrow
+            case 'ArrowLeft':
+                this.babyPosition.left = Math.floor( this.babyPosition.left ) - 10;
+                babyIcon.className = '';
+                babyIcon.classList.add( 'going-left' );
+                babyIcon.style.left = this.babyPosition.left.toString() + 'px';
+
+                break;
+            // right arrow
+            case 'ArrowRight':
+                this.babyPosition.left = Math.floor( this.babyPosition.left ) + 10;
+                babyIcon.className = '';
+                babyIcon.classList.add( 'going-right' );
+                babyIcon.style.left = this.babyPosition.left.toString() + 'px';
+                break;
+        }
+        console.log( 'babyIcon', babyIcon, 'babyPosition', this.babyPosition );
+    }
+
+    addToScore() {
+    }
+
+    isTouching() {
+
+    }
 }
+
+const screenBaby = new baby( 'Toby' );
+
+babyIcon.addEventListener( 'click', () => {
+    console.log( 'Baby clicked' );
+} );
+
+
+window.addEventListener( 'keydown', ( e ) => {
+    screenBaby.move( e )
+} )

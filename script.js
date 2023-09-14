5824ac7b1d05808e715a8eba54da10761edc30a5
iansexton <mydesignguy@gmail.com>
2023-09-13 22:16:38 -0400

    [working] with sound effects

--- script.js
+++ script.js
@@ -6,17 +6,18 @@
  */
 const items = document.querySelectorAll('.item');
 const babyIcon = document.getElementById('baby');
+
+// set baby starting position
+babyIcon.style.top = 400 + 'px';
+babyIcon.style.left = 490 + 'px';
 const objLocationMap = {};
-const opposites = {
-  left: 'right',
-  right: 'left',
-  top: 'bottom',
-  bottom: 'top',
-};
 
 // SOUNDS
 const s_boing = new Audio('sounds/boing.mp3');
-const s_bonk_deep = new Audio('sounds/bonk-deep.mp3');
+const s_collision = new Audio('sounds/collision.mp3');
+const s_wah = new Audio('sounds/wah.mp3');
+const s_crying = new Audio('sounds/crying.mp3');
+const s_laugh = new Audio('sounds/giggle.mp3');
 const s_crash = new Audio('sounds/crash-loud-short.mp3');
 const s_end = new Audio('sounds/endgame.mp3');
 const s_glass_break = new Audio('sounds/glass-break-short.mp3');
@@ -51,15 +52,27 @@ class baby {
     babyIcon.className = '';
     babyIcon.classList.add('going-' + direction);
     if (direction === 'up' || direction === 'down') {
-      babyIcon.style.top = this.babyPos.top.toString() + 'px';
+      babyIcon.style.top = this.babyPos.top + 'px';
     } else if (direction === 'left' || direction === 'right') {
-      babyIcon.style.left = this.babyPos.left.toString() + 'px';
+      babyIcon.style.left = this.babyPos.left + 'px';
     }
+  }
+
+  cry() {
+    setTimeout(() => {
+      console.log('enter cry');
+      document.getElementById('babyImg').src = 'img/baby-cry.webp';
+      s_crying.play();
+    }, 100);
+    setTimeout(() => {
+      document.getElementById('babyImg').src = 'img/baby.webp';
+      s_laugh.play();
+    }, 4300);
   }
 
   move(e) {
     e = e || window.Event;
-    console.log('moving', e.key);
+    //console.log('moving', e.key);
     switch (e.key) {
       // up arrow
       case 'ArrowUp':
@@ -82,34 +95,57 @@ class baby {
         this.setDirectionClass('right');
         break;
       default:
-        console.log('something is wrong in the direction switch. Keypress was:', e.key);
+        throw new Error('Something is wrong in the direction switch, user probably pressed not an arrow key. Do nothing. Keypress was:', e.key);
         break;
     }
     this.babyPos.right = babyIcon.getBoundingClientRect().right;
-    this.babyPos.bottom = babyIcon.getBoundingClientRect().bottom;
+    this.babyPos.bottom = babyIcon.getBoundingClientRect().top;
     this.isTouching();
   }
 
   addToScore() {}
 
-  stopBaby() {
+  bounceBabyBack() {
     // get the word for current direction, i., right, left, etc.
     let currentDir = babyIcon.className.slice(babyIcon.className.indexOf('-') + 1);
     console.log('Stop Baby:');
-    console.log('currentDir', currentDir);
+    console.log('currentDir', currentDir, this.babyPos);
 
     // since we're only operating on x and y, treat up and down as one direction, and left and right as the other.
     switch (currentDir) {
       case 'left':
+        console.log('bounce right 10 px from', this.babyPos.left);
+        s_collision.play();
+        this.cry();
+        this.babyPos.left = Math.round(this.babyPos.left + 30);
+        babyIcon.style.left = this.babyPos.left + 'px';
+        break;
+      case 'right':
+        s_collision.play();
+        this.cry();
+        console.log('bounce left 10 px from', this.babyPos.left);
+        this.babyPos.right = Math.round(this.babyPos.left - 30);
+        break;
+      case 'up':
+        s_collision.play();
+        this.cry();
+        console.log('bounce top 10 px from', this.babyPos.top);
+        this.babyPos.top = Math.round(this.babyPos.top + 30);
+        break;
+      case 'down':
+        s_collision.play();
+        this.cry();
+        console.log('bounce bottom 10 px from', this.babyPos.top);
+        this.babyPos.bottom = Math.round(this.babyPos.top - 30);
+        break;
+      default:
+        throw new Error("Oops, there's a bouneback issue. Can't tell where he's coming from.");
         break;
     }
-    if (currentDir === 'left' || currentDir === 'right') {
-      if (currentDir === 'left') {
-      } else {
-      }
-      this.babyPos.left = Math.round(this.babyPos[opposites[currentDir]] + 10);
-    } else if (currentDir === 'up' || currentDir === 'down') {
-    }
+
+    this.babyPos.right = babyIcon.getBoundingClientRect().right;
+    this.babyPos.bottom = babyIcon.getBoundingClientRect().bottom;
+
     // this.babyPos[opposites[currentDir]]=
     // 1. get the (going-)right direction from class -- OK
     // 2. bounce him in the opposite direction 10px, top or left, +/-
@@ -133,12 +169,12 @@ class baby {
       if (horizontalOverlap && verticalOverlap) {
         console.log('😳 👼🏻  Baby is colliding with item:', item.dataset.name);
         isColliding = true;
-        this.stopBaby();
+        this.bounceBabyBack();
       }
     });
 
     if (!isColliding) {
-      console.log('Baby is not colliding with any item.');
+      //console.log('Baby is not colliding with any item.');
     }
   }
 }
